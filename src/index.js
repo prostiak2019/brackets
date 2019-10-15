80d2e07e25204ba3794b04d702581b513a983e5a
prostiak2019 <yarovenkodmv@gmail.com>
2019-10-15 19:05:06 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -3,27 +3,26 @@ module.exports = function check(str, bracketsConfig) {
     // create the array of stringified config items
     const config = bracketsConfig.map(item => item.join(''));
   
-    // loop input string to remove pairs of opening/closing items from config
+    // loop input string to remove pairs of opening/closing items from 'config'
     while (str.length) {
   
       // add flag to control exit from the loop
       let extend = false;
   
-      // loop config items to remove them from the input string
+      // loop 'config' items to remove them from the input string
       for (let item of config) {
         if (str.includes(item)) {
           str = str.replace(item, '');
             
-          console.log (str)
           // return true if the string has been cleaned
           if (!str.length) return true;
   
-          // set the flag to true if at least one item from the config has been found in the string
+          // set the flag to true if at least one item from the 'config' has been found in the string
           extend = true;
         }
       }
   
-      // return false if the extend flag is false
+      // return false if the 'extend' flag is false
       if (!extend) return false;
     }
   }
